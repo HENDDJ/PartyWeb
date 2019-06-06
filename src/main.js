b93436b4d6bc7cfd9ff026776b4098d6847dbf57
asher <243485908@qq.com>
2019-06-06 13:56:00 +0800

    界面调整

--- src/main.js
+++ src/main.js
@@ -19,6 +19,7 @@ import Vuesax from 'vuesax'
 import 'material-icons/iconfont/material-icons.css';
 import 'vuesax/dist/vuesax.css' //Vuesax styles
 import Spinner from 'vue-spinkit'
+
 Vue.use(Vuesax)
 Vue.component('Spinner', Spinner)
 

f013c56e04534ed827a5beb6daca7d7029b7f9f9
cqhssh <1170669705@qq.com>
2019-07-25 18:09:38 +0800

    完善

--- src/views/menu/Statistics.vue
+++ src/views/menu/Statistics.vue
@@ -6,9 +6,9 @@
                 v-model="year"
                 align="right"
                 type="year"
-                placeholder="选择年" @change="loadTables">
+                placeholder="选择年" @change="loadTables()">
             </el-date-picker>
-            <el-select v-model="districtId" placeholder="请选择" @change="loadTables" size="medium">
+            <el-select v-model="districtId" placeholder="请选择" @change="loadTables()" size="medium">
                 <el-option
                     v-for="item in districtList"
                     :key="item.value"
@@ -19,11 +19,11 @@
             <el-button type="primary" icon="el-icon-printer" size="small">打印</el-button>
         </div>
         <div border="1px grey solids">
-            <div style="overflow-x: auto; overflow-y: auto;text-align:left;width: 100%;">
-                <h4 style="text-align: center">活动完成情况一览表</h4>
-                <table style="width:100px;display: inline-block; vertical-align: top;text-align: center">
+            <div class="contentDiv" >
+                <h3>{{toneName+"活动完成情况一览表"}}</h3>
+                <table class="tableCol" >
                     <tr>
-                        <td valign="top" style="height: 100px;font-size: 16px">
+                        <td>
                             <div class="tableline">
                                 <p style="line-height: 50px; text-align: right;padding-right: 10px;">任务</p>
                                 <p style="line-height: 50px; text-align: left;padding-left: 10px;">村名</p>
@@ -31,24 +31,29 @@
                         </td>
                     </tr>
                     <tr v-for="(value,key) in activityList" v-if="key!='title'">
-                        <td style="background-color: #f3f3f3; width: 110px;font-family:' 黑体';font-weight:bold;line-height:25px;font-size: 12px;height: 25px;"  valign="top"> {{key}}</td>
+                        <td class="tableColContent"> {{key}}</td>
                     </tr>
                 </table>
-               <table style="width: 61.4%; display: inline-block;overflow: scroll;text-align: center">
+               <table class="tableContent">
                    <tr>
-                       <td v-for="item in activityList.title" style="background-color: #0dc9ce;"><button type="text" style="height: 100px;width: 100px;padding:3px 3px;border-radius:4px;background-color: #F4F4F4;font-size: 14px">{{item}}</button></td>
+                       <td v-for="item in activityList.title" style="background-color: #0dc9ce;">
+                           <button type="text" >{{item}}</button>
+                       </td>
                    </tr>
                    <tr v-for="(value,key) in activityList"  v-if="key!='title'">
-                       <td v-for="(index,item) in value" style="width: 100px;font-size: 12px;background-color: #39c667;color:white;height: 25px;"  v-if="index.status=='2'">
-                           已完成
+                       <td class="content" v-for="(index,item) in value" style="background-color: #39c667"v-if="index.status=='2'" >
+                           <div @click="showPictures(index)">已完成</div>
                        </td>
-                       <td v-for="(index,item) in value"  style="width: 100px;font-size: 12px;background-color: #DC143C;color: white;height: 25px;"  v-if="index.status!='2'">
+                       <td class="content" v-for="(index,item) in value"  style="background-color: #DC143C;"  v-if="index.status!='2'">
                           未完成
                        </td>
                    </tr>
                </table>
-                <div style="width: 30%;border: 1px black solid;display: inline-block;vertical-align: top;height: 500px;">
-
+                <div class="pictureshow" >
+                    <h4>{{countryName+"活动执行截图"}}</h4><br/>
+                    <div v-if="tipShow">请选择需要查看的任务记录！</div>
+                    <div id="div-with-loading" class="vs-con-loading__container" v-show="!pictureShow"></div>
+                    <PictureShot :picData="picQuery" v-if="pictureShow" ></PictureShot>
                 </div>
             </div>
         </div>
@@ -56,6 +61,7 @@
 </template>
 
 <script>
+    import PictureShot from '@/components/PictureShot';
     export default {
         name: "Statistics",
         data(){
@@ -64,14 +70,20 @@
                 year:'',
                 districtId:'01',
                 districtList:[],
+                organizationId: '',
+                activityId: '',
+                pictureShow:false,
+                countryName:'',
+                toneName:'句容市委',
+                tipShow:true,
             }
         },
-
         methods:{
             loadTables(){
                 if(!this.year){
                     this.year=new Date();
                 }
+                this.toneName = this.districtList.filter( item => item.value==this.districtId)[0].label;
                 this.$http('post',`identity/parActivity/list/completion?year=${new Date(this.year).Format("yyyy")}&districtId=${this.districtId}`,false).then( data => {
                     this.activityList = data;
                 });
@@ -84,13 +96,43 @@
                         this.districtList.push( {value:item.districtId , label:item.districtName});
                     });
                     this.districtList.push({value:'01',label:'句容市委'});
+                    this.loadTables();
                 })
-                console.log(this.districtList)
+
+            },
+            showPictures(item){
+                this.tipShow = false;
+                this.pictureShow=false;
+                this.$vs.loading({
+                    container: '#div-with-loading',
+                    scale: 0.9
+                })
+                this.countryName = item.districtName;
+                this.organizationId = item.organizationId;
+                this.activityId = item.activityId;
+                this.$nextTick( () => {
+                    setTimeout( ()=> {
+                        this.$vs.loading.close('#div-with-loading > .con-vs-loading')
+                        this.pictureShow = true;
+                    }, 1000);
+                })
+
+            }
+        },
+        components: {
+            PictureShot
+        },
+        computed:{
+            picQuery() {
+                return {
+                    districtId: this.organizationId,
+                    activityId: this.activityId
+                }
             }
         },
         created() {
-            this.loadTables();
             this.showDistrictList();
+
         }
     }
 </script>
@@ -122,5 +164,72 @@
         border-bottom:1px solid deeppink;
         transform-origin:bottom center;
         transform:rotateZ(45deg) scale(1.414);
+    }
+    #div-with-loading{
+        background-color: transparent!important;
+        width: 200px;
+        height: 200px;
+        margin: auto;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 5px;
+    }
+    .pictureshow{
+        width: 30%;
+        display: inline-block;
+        vertical-align: top;
+    }
+    .contentDiv{
+        overflow-x: auto;
+        text-align:center;
+        width: 100%;
+    }
+    .tableCol{
+        width:100px;
+        display: inline-block;
+        vertical-align: top;
+    }
+    .tableCol td{
+        height: 100px;
+        font-size: 16px;
+    }
+    .tableColContent{
+        background-color: #f3f3f3;
+        width: 110px;
+        font-family:' 黑体';
+        font-weight:bold;
+        line-height:25px;
+        font-size: 12px!important;
+        height: 25px!important;
+    }
+    .tableContent{
+        width: 61.4%;
+        display: inline-block;
+        overflow: scroll;
+        text-align: center;
+    }
+
+    .tableContent button{
+        height: 100px;
+        width: 100px;
+        padding:3px 3px;
+        border-radius:4px;
+        background-color: #F4F4F4;
+        font-size: 14px;
+    }
+    .tableContent .content{
+        width: 100px;
+        font-size: 12px;
+        color: white;
+        height: 25px;
+    }
+    .tableContent .content :hover{
+        cursor: pointer;
+    }
+</style>
+<style>
+    .pictureshow .con-vs-loading{
+        background: transparent!important;
     }
 </style>

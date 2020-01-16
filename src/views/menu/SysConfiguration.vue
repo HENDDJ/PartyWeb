55e20245df275da7c7b1b3bcc4741d511c73224c
asher <243485908@qq.com>
2020-01-16 11:23:29 +0800

    权限管理
    (cherry picked from commit 0b95ab5)

--- src/views/menu/SysConfiguration.vue
+++ src/views/menu/SysConfiguration.vue
@@ -1,31 +1,64 @@
 <template>
     <section>
-        <el-row :gutter="12">
-            <el-col :span="6">
-                <el-card shadow="hover">
-                   <img src="@/svg/testuser.svg" class="image"/>
-                    <div class="labelPosiotn">{{testUser.name}}<br/><br/>
-                        <el-switch
-                            v-model="testUser.codeValue"
-                            active-text="可用"
-                            inactive-text="不可用"
-                            active-value="0"
-                            inactive-value="1"
-                            @change="handleTestUser()"
-                        style="height: 27px;">
-                        </el-switch>
-                    </div>
-                </el-card>
-            </el-col>
-            <el-col :span="6">
-                <el-card shadow="hover">
-                    <img src="@/svg/time2.svg" class="image"/>
-                    <div class="labelPosiotn">{{shotScreen.name}}<br/><br/>
-                        <vs-input-number @click="updateShotTime()" color="success" v-model="shotScreen.codeValue"/>
-                    </div>
-                </el-card>
-            </el-col>
-        </el-row>
+        <div>
+            <el-card shadow="hover">
+                <el-row>
+                    <el-col :span="12">
+                        <img src="@/svg/testuser.svg" class="image"/>
+                    </el-col>
+                    <el-col :span="12">
+                        <div class="labelPosiotn">{{testUser.name}}</div>
+                        <div style="text-align: left">
+                            <el-switch
+                                v-model="testUser.codeValue"
+                                active-text="可用"
+                                inactive-text="不可用"
+                                active-value="0"
+                                inactive-value="1"
+                                @change="handleTestUser()"
+                                style="height: 27px;">
+                            </el-switch>
+                        </div>
+                    </el-col>
+                </el-row>
+            </el-card>
+            <el-card shadow="hover">
+                <el-row>
+                    <el-col :span="12">
+                        <img src="@/svg/time2.svg" class="image"/>
+                    </el-col>
+                    <el-col :span="12">
+                        <div class="labelPosiotn">{{shotScreen.name}}</div>
+                        <div style="text-align: left">
+                            <vs-input-number @click="updateShotTime()" color="success" v-model="shotScreen.codeValue"/>
+                        </div>
+                    </el-col>
+                </el-row>
+
+
+            </el-card>
+            <el-card shadow="hover" v-for= "item in authTokenList" :key="item.id">
+                <el-row>
+                    <el-col :span="12">
+                        <img src="@/svg/auth.svg" class="image"/>
+                    </el-col>
+                    <el-col :span="12">
+                        <div class="labelPosiotn"><b>{{item.name}}</b></div>
+                        <div style="text-align: left">
+                            <el-switch
+                                v-model="item.isUse"
+                                active-text="可访问"
+                                inactive-text="不可访问"
+                                active-value="1"
+                                inactive-value="0"
+                                @change="handleAuthToken(item)"
+                                style="height: 27px;">
+                            </el-switch>
+                        </div>
+                    </el-col>
+                </el-row>
+            </el-card>
+        </div>
     </section>
 
 </template>
@@ -41,7 +74,8 @@
                     name:'',
                     code:'',
                     codeValue:''
-                }
+                },
+                authTokenList:[],
             }
         },
         methods:{
@@ -57,15 +91,24 @@
                     })
                 })
             },
-
             handleTestUser(){
-                this.$http("PUT", `identity/sysConfiguration/${this.testUser.id}isDelete`,this.testUser, false).then(() => {
+                this.$http("PUT", `identity/sysConfiguration/${this.testUser.id}isDelete`,this.testUser).then(() => {
                     this.showConfigList();
                 })
             },
             updateShotTime(){
-                this.$http("PUT",`identity/sysConfiguration/${this.shotScreen.id}id`,this.shotScreen,false).then( ()=> {
+                this.$http("PUT",`identity/sysConfiguration/${this.shotScreen.id}id`,this.shotScreen).then( ()=> {
                     this.showConfigList();
+                })
+            },
+            showAuthTokenList(){
+                this.$http("POST",`/identity/authorizationToken/list`,false).then( (data)=> {
+                    this.authTokenList = data;
+                })
+            },
+            handleAuthToken(item){
+                this.$http("POST", `/identity/authorizationToken/updateRedis/${item.id}id/${item.isUse}`).then(() => {
+                    this.showAuthTokenList();
                 })
             }
 
@@ -82,7 +125,8 @@
             },
         },
         created(){
-            this.showConfigList()
+            this.showConfigList();
+            this.showAuthTokenList();
         }
     }
 </script>
@@ -92,8 +136,10 @@
         background: transparent;
     }
     .el-card{
-        width: 350px;
+        width: 360px;
         height: 160px;
+        float: left;
+        margin: 20px;
     }
     .image{
         float: left;
@@ -103,7 +149,8 @@
     .labelPosiotn{
         font-size: 14px;
         text-align: left;
-        margin: 32px 12px;
+        margin-top: 32px;
+        margin-bottom: 5px;
     }
     .vs-input-number {
         justify-content: left;
